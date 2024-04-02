10bf83d53c5bde4299c44fbd2611f812dd632ddc
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-02 23:51:10 +0100

    final group page

--- cm2104-2324-groupproject-jp/final/scripts/newgroups.js
+++ cm2104-2324-groupproject-jp/final/scripts/newgroups.js
@@ -18,3 +18,38 @@ function showAccountContent(page) {
         selectedContainer.style.display = 'block';
     }
 }
+
+function showGroupContent(page) {
+  var groupSelectorChat = document.querySelectorAll('.group-selector-container-chat');
+  var groupSelectorWatchlist = document.querySelectorAll('.group-selector-container-watchlist');
+  var watchlistContainers = document.querySelectorAll('.watchlist-container');
+  var chatContainers = document.querySelectorAll('.chat-container');
+
+  if (page === 'watchlists') {
+      groupSelectorChat.forEach(function(container) {
+          container.style.display = 'block';
+      });
+      groupSelectorWatchlist.forEach(function(container) {
+          container.style.display = 'none';
+      });
+      watchlistContainers.forEach(function(container) {
+          container.style.display = 'block';
+      });
+      chatContainers.forEach(function(container) {
+          container.style.display = 'none';
+      });
+  } else if (page === 'chat') {
+      groupSelectorChat.forEach(function(container) {
+          container.style.display = 'none';
+      });
+      groupSelectorWatchlist.forEach(function(container) {
+          container.style.display = 'block';
+      });
+      watchlistContainers.forEach(function(container) {
+          container.style.display = 'none';
+      });
+      chatContainers.forEach(function(container) {
+          container.style.display = 'block';
+      });
+  }
+}
