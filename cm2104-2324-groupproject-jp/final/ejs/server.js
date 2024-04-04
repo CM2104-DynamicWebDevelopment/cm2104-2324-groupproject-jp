f7c765511026648581acfc32543ebc75646f250d
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-04 21:58:25 +0100

    server

--- cm2104-2324-groupproject-jp/final/ejs/server.js
+++ cm2104-2324-groupproject-jp/final/ejs/server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+/**
+ * @Author: John Isaacs <john>
+ * @Date:   01-Mar-19
+ * @Filename: server.js
+ * @Last modified by:   john
+ * @Last modified time: 03-Mar-2024
+ */
+
+const express = require('express'); // npm install express
+const session = require('express-session'); // npm install express-session
+const bodyParser = require('body-parser'); // npm install body-parser
 const path = require('path');
 
 const app = express();
-const PORT = 8080; // Change port to 8080
+const PORT = 8080; // Change port to the desired port number
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'your-secret-key', // Change this to a secret key
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(session({ secret: 'example' }));
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -21,15 +25,22 @@ app.set('view engine', 'ejs');
 // Set the path to serve static files (like CSS, images)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set the path for EJS views
-app.set('views', path.join(__dirname, 'views'));
-
 // Routes
+
+// Route to render the index.ejs page
 app.get('/', (req, res) => {
-  res.render('index', { user: req.session.user }); // Passing user data to EJS template
+  res.render('index', { user: req.session.user });
 });
 
-// Add more routes as needed...
+// Route to render the myaccount.ejs page
+app.get('/myaccount', (req, res) => {
+  res.render('myaccount', { user: req.session.user });
+});
+
+// Route to render the group.ejs page
+app.get('/groups', (req, res) => {
+  res.render('groups', { user: req.session.user });
+});
 
 // Start the server
 app.listen(PORT, () => {
