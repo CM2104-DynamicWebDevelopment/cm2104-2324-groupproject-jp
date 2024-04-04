53b581fd735c3082f6b29d0396e2a93b9d2b42e2
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-04 17:32:58 +0100

    ejs

---
+++ cm2104-2324-groupproject-jp/final/ejs/scripts/newgroups.js
@@ -0,0 +1,55 @@
+/*
+  Created by Michael Piercey 2206222 -- finalised 11/12/23
+*/
+
+
+// nav bar for showing accout, review, watchlist and freinds, accepts the page user wants to view
+function showAccountContent(page) {
+    // this hides all containers
+    document.querySelectorAll('.account-container').forEach(element => {
+      // by setting dispaly to none the container does not appear
+        element.style.display = 'none';
+    });
+
+    // this then uses input to show selected container
+    var selectedContainer = document.getElementById(`container-${page}`);
+    if (selectedContainer) {
+      // sets display to block instead of none
+        selectedContainer.style.display = 'block';
+    }
+}
+
+function showGroupContent(page) {
+  var groupSelectorChat = document.querySelectorAll('.group-selector-container-chat');
+  var groupSelectorWatchlist = document.querySelectorAll('.group-selector-container-watchlist');
+  var watchlistContainers = document.querySelectorAll('.watchlist-container');
+  var chatContainers = document.querySelectorAll('.chat-container');
+
+  if (page === 'watchlists') {
+      groupSelectorChat.forEach(function(container) {
+          container.style.display = 'block';
+      });
+      groupSelectorWatchlist.forEach(function(container) {
+          container.style.display = 'none';
+      });
+      watchlistContainers.forEach(function(container) {
+          container.style.display = 'block';
+      });
+      chatContainers.forEach(function(container) {
+          container.style.display = 'none';
+      });
+  } else if (page === 'chat') {
+      groupSelectorChat.forEach(function(container) {
+          container.style.display = 'none';
+      });
+      groupSelectorWatchlist.forEach(function(container) {
+          container.style.display = 'block';
+      });
+      watchlistContainers.forEach(function(container) {
+          container.style.display = 'none';
+      });
+      chatContainers.forEach(function(container) {
+          container.style.display = 'block';
+      });
+  }
+}
