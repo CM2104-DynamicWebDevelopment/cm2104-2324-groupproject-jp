34cfc5b22ba8e42416d952b28e86cf184eebb488
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-04 21:45:25 +0100

    server

--- cm2104-2324-groupproject-jp/final/ejs/server.js
+++ cm2104-2324-groupproject-jp/final/ejs/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = 8080; // Change port to 8080
@@ -17,50 +18,15 @@ app.use(session({
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
+// Set the path to serve static files (like CSS, images, or your EJS views)
+app.use(express.static(path.join(__dirname, 'final/ejs/views')));
+
 // Routes
 app.get('/', (req, res) => {
   res.render('index', { user: req.session.user }); // Passing user data to EJS template
 });
 
-app.get('/myaccount', (req, res) => {
-  // Check if the user is logged in
-  if (req.session.user) {
-    res.render('myaccount');
-  } else {
-    res.redirect('/');
-  }
-});
-
-app.get('/groups', (req, res) => {
-  // Check if the user is logged in
-  if (req.session.user) {
-    res.render('groups');
-  } else {
-    res.redirect('/');
-  }
-});
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  // Assuming a simple authentication mechanism
-  if (username === 'admin' && password === 'password') {
-    req.session.user = username; // Storing user data in session
-    res.redirect('/');
-  } else {
-    res.send('Invalid username or password');
-  }
-});
-
-app.get('/logout', (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      console.error('Error destroying session:', err);
-      res.sendStatus(500);
-    } else {
-      res.redirect('/');
-    }
-  });
-});
+// Add more routes as needed...
 
 // Start the server
 app.listen(PORT, () => {
