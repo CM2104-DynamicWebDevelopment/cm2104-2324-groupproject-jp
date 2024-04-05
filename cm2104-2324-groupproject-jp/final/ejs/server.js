c8ef840b35e7418deda1e1687949254485df8850
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-05 18:24:58 +0100

    test

--- cm2104-2324-groupproject-jp/final/ejs/server.js
+++ cm2104-2324-groupproject-jp/final/ejs/server.js
@@ -42,49 +42,33 @@ app.use(favicon(path.join(__dirname, 'public', 'img', 'cinemind_small_logo.png')
 
 // Route to render the index.ejs page
 app.get('/', (req, res) => {
-    if (!req.session.loggedin) {
-        res.redirect('/login');
-        return;
+    // If user is logged in, render index page
+    if (req.session.loggedin) {
+        res.render('pages/index', { user: req.session.user });
+    } else {
+        // If not logged in, render index page with login form
+        res.render('pages/index', { user: null });
     }
-    res.render('pages/index', { user: req.session.user });
 });
 
-// Route to render the login.ejs page
-app.get('/login', (req, res) => {
-    res.render('pages/login');
-});
-
-// Route to render the profile.ejs page
-app.get('/profile', (req, res) => {
+// Route to render the myaccount.ejs page
+app.get('/myaccount', (req, res) => {
+    // Redirect to login if not logged in
     if (!req.session.loggedin) {
-        res.redirect('/login');
+        res.redirect('/');
         return;
     }
-
-    const uname = req.query.username;
-
-    db.collection('people').findOne({ "login.username": uname }, (err, result) => {
-        if (err) throw err;
-        res.render('pages/profile', { user: result });
-    });
+    res.render('pages/myaccount', { user: req.session.user });
 });
 
-// Route to render the adduser.ejs page
-app.get('/adduser', (req, res) => {
+// Route to render the group.ejs page
+app.get('/groups', (req, res) => {
+    // Redirect to login if not logged in
     if (!req.session.loggedin) {
-        res.redirect('/login');
+        res.redirect('/');
         return;
     }
-    res.render('pages/adduser');
-});
-
-// Route to render the remuser.ejs page
-app.get('/remuser', (req, res) => {
-    if (!req.session.loggedin) {
-        res.redirect('/login');
-        return;
-    }
-    res.render('pages/remuser');
+    res.render('pages/groups', { user: req.session.user });
 });
 
 // Route to handle login form submission
@@ -96,16 +80,17 @@ app.post('/dologin', (req, res) => {
         if (err) throw err;
 
         if (!result) {
-            res.redirect('/login');
+            res.redirect('/');
             return;
         }
 
         if (result.login.password == pword) {
             req.session.loggedin = true;
             req.session.currentuser = uname;
+            req.session.user = result; // Store user data in session
             res.redirect('/');
         } else {
-            res.redirect('/login');
+            res.redirect('/');
         }
     });
 });
@@ -113,7 +98,7 @@ app.post('/dologin', (req, res) => {
 // Route to handle user deletion
 app.post('/delete', (req, res) => {
     if (!req.session.loggedin) {
-        res.redirect('/login');
+        res.redirect('/');
         return;
     }
     const uname = req.body.username;
@@ -127,7 +112,7 @@ app.post('/delete', (req, res) => {
 // Route to handle adding a new user
 app.post('/adduser', (req, res) => {
     if (!req.session.loggedin) {
-        res.redirect('/login');
+        res.redirect('/');
         return;
     }
 
