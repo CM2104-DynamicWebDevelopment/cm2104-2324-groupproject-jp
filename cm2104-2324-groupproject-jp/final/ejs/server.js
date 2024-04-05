acc71a0796b29d0dd99b08567d7b384b14419249
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-06 00:09:52 +0100

    test

--- cm2104-2324-groupproject-jp/final/ejs/server.js
+++ cm2104-2324-groupproject-jp/final/ejs/server.js
@@ -54,19 +54,7 @@ app.get('/myaccount', (req, res) => {
       res.redirect('/');
       return;
   }
-  
-  const currentUserId = req.session.userId;
-
-  // Retrieve the watchlist of the currently logged-in user
-  getWatchlist(currentUserId)
-      .then(watchlist => {
-          // Render myaccount page with the watchlist
-          res.render('pages/myaccount', { watchlist });
-      })
-      .catch(err => {
-          console.error('Error fetching watchlist:', err);
-          res.status(500).send('Error fetching watchlist');
-      });
+  res.render('pages/myaccount', { user: req.session.user });
 });
 
 
@@ -166,20 +154,3 @@ app.post('/addwatchlist', (req, res) => {
 
 
 
-function getWatchlist(userId) {
-  return new Promise((resolve, reject) => {
-      // Find the user document in the database
-      db.collection('people').findOne({ _id: userId }, (err, user) => {
-          if (err) {
-              reject(err); // Reject promise if there's an error
-              return;
-          }
-          if (!user) {
-              reject(new Error('User not found')); // Reject promise if user not found
-              return;
-          }
-          // Resolve promise with user's watchlist
-          resolve(user.watchlist);
-      });
-  });
-}
