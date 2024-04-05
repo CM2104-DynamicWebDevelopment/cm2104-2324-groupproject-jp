134594a6925054e9be84fff19369211735527a8e
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-05 18:22:47 +0100

    ds

--- cm2104-2324-groupproject-jp/final/ejs/server.js
+++ cm2104-2324-groupproject-jp/final/ejs/server.js
@@ -1,17 +1,9 @@
-/**
- * @Author: John Isaacs <john>
- * @Date:   01-Mar-19
- * @Filename: server.js
- * @Last modified by:   john
- * @Last modified time: 03-Mar-2024
- */
-
-const express = require('express'); // npm install express
-const session = require('express-session'); // npm install express-session
-const bodyParser = require('body-parser'); // npm install body-parser
+// Import required modules
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const path = require('path');
 const favicon = require('serve-favicon');
-
 
 const app = express();
 const PORT = 8080; // Change port to the desired port number
@@ -24,26 +16,152 @@ app.use(session({ secret: 'example' }));
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
+// Connect to MongoDB
+const MongoClient = require('mongodb-legacy').MongoClient;
+const url = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(url);
+const dbname = 'profiles';
+
+let db;
+
+// Connect to MongoDB and start server
+async function connectDB() {
+    await client.connect();
+    console.log('Connected successfully to server');
+    db = client.db(dbname);
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+connectDB();
+
 // Routes
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'img', 'cinemind_small_logo.png')));
 
 // Route to render the index.ejs page
 app.get('/', (req, res) => {
-  res.render('pages/index', { user: req.session.user });
+    if (!req.session.loggedin) {
+        res.redirect('/login');
+        return;
+    }
+    res.render('pages/index', { user: req.session.user });
 });
 
-// Route to render the myaccount.ejs page
-app.get('/myaccount', (req, res) => {
-  res.render('pages/myaccount', { user: req.session.user });
+// Route to render the login.ejs page
+app.get('/login', (req, res) => {
+    res.render('pages/login');
 });
 
-// Route to render the group.ejs page
-app.get('/groups', (req, res) => {
-  res.render('pages/groups', { user: req.session.user });
+// Route to render the profile.ejs page
+app.get('/profile', (req, res) => {
+    if (!req.session.loggedin) {
+        res.redirect('/login');
+        return;
+    }
+
+    const uname = req.query.username;
+
+    db.collection('people').findOne({ "login.username": uname }, (err, result) => {
+        if (err) throw err;
+        res.render('pages/profile', { user: result });
+    });
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Route to render the adduser.ejs page
+app.get('/adduser', (req, res) => {
+    if (!req.session.loggedin) {
+        res.redirect('/login');
+        return;
+    }
+    res.render('pages/adduser');
+});
+
+// Route to render the remuser.ejs page
+app.get('/remuser', (req, res) => {
+    if (!req.session.loggedin) {
+        res.redirect('/login');
+        return;
+    }
+    res.render('pages/remuser');
+});
+
+// Route to handle login form submission
+app.post('/dologin', (req, res) => {
+    const uname = req.body.username;
+    const pword = req.body.password;
+
+    db.collection('people').findOne({ "login.username": uname }, (err, result) => {
+        if (err) throw err;
+
+        if (!result) {
+            res.redirect('/login');
+            return;
+        }
+
+        if (result.login.password == pword) {
+            req.session.loggedin = true;
+            req.session.currentuser = uname;
+            res.redirect('/');
+        } else {
+            res.redirect('/login');
+        }
+    });
+});
+
+// Route to handle user deletion
+app.post('/delete', (req, res) => {
+    if (!req.session.loggedin) {
+        res.redirect('/login');
+        return;
+    }
+    const uname = req.body.username;
+
+    db.collection('people').deleteOne({ "login.username": uname }, (err, result) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
+});
+
+// Route to handle adding a new user
+app.post('/adduser', (req, res) => {
+    if (!req.session.loggedin) {
+        res.redirect('/login');
+        return;
+    }
+
+    const datatostore = {
+        "gender": req.body.gender,
+        "name": {
+            "title": req.body.title,
+            "first": req.body.first,
+            "last": req.body.last
+        },
+        "location": {
+            "street": req.body.street,
+            "city": req.body.city,
+            "state": req.body.state,
+            "postcode": req.body.postcode
+        },
+        "email": req.body.email,
+        "login": {
+            "username": req.body.username,
+            "password": req.body.password
+        },
+        "dob": req.body.dob,
+        "registered": Date(),
+        "picture": {
+            "large": req.body.large,
+            "medium": req.body.medium,
+            "thumbnail": req.body.thumbnail
+        },
+        "nat": req.body.nat
+    };
+
+    db.collection('people').save(datatostore, (err, result) => {
+        if (err) throw err;
+        console.log('saved to database');
+        res.redirect('/');
+    });
 });
