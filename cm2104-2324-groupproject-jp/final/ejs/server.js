44e06725b2006f7777efe1449056ca3ce9a8d349
Michael Piercey <m.piercey@rgu.ac.uk>
2024-04-09 09:52:06 +0100

    test

--- cm2104-2324-groupproject-jp/final/ejs/server.js
+++ cm2104-2324-groupproject-jp/final/ejs/server.js
@@ -167,6 +167,7 @@ app.post('/addwatchlist', (req, res) => {
     }
 
     const watchlist = req.session.user.watchlist;
+    const userId = req.session.userId;
     const userEmail = req.session.user.email; // Retrieve user email from session
 
     if (watchlist.movieIds.includes(movieId)) {
