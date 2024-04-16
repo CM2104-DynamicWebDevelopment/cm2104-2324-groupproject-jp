518699d8e7beda2c715dff50dcc79995c8932581
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-16 23:49:22 +0100

    test

--- cm2104-2324-groupproject-jp/final/ejs/public/scripts/home.js
+++ cm2104-2324-groupproject-jp/final/ejs/public/scripts/home.js
@@ -185,13 +185,15 @@ $(document).ready(function () {
             "<div class='rating-buttons'>" +
               "<form id='reviewForm' action='/addreview' method='POST'>" +
                 "<input type='hidden' name='movieId' value='" + id + "'>" +
+                "<div class='button-group'>" +
                 "<h6>number review</h6>"+
-                "<select name='rating'>" + 
-                "<option value='4'>4</option>" +
-                "<option value='4'>4</option>" +
-                "<option value='4'>4</option>"+
+                "<select name='rating'  id='rating'>" + 
+                "<option value='1'>1</option>" +
+                "<option value='2'>2</option>" +
+                "<option value='3'>3</option>"+
                 "<option value='4'>4</option>" +
                 "</select>" +
+                "</div>" +
                 "<div class='review-section'>" +
                   "<label class='comments-review'>Have your say:</label>" +
                   "<textarea class='review-section-text' rows='4' name='review'></textarea>" +
