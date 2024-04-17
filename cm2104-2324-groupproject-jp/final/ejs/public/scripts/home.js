5205fc133b847ee94e6d1997dd769fd42c4d6f10
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-17 20:10:56 +0100

    rwar

--- cm2104-2324-groupproject-jp/final/ejs/public/scripts/home.js
+++ cm2104-2324-groupproject-jp/final/ejs/public/scripts/home.js
@@ -325,7 +325,7 @@ htmlString += "<div class='movie-details'>" +
                         "<option value='1'>1</option>" +
                         "<option value='2'>2</option>" +
                         "<option value='3'>3</option>"+
-                        "<option value='4'>4</option>"
+                        "<option value='4'>4</option>" +
                         "<option value='5'>5</option>" +
                     "</select>" +
                     "<div class='review-section'>" +
