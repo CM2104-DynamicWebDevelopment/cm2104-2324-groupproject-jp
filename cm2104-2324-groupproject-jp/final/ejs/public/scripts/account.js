38a49c11d88da48320d468fb2eb5bcafbdc07411
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-17 14:22:59 +0100

    test

--- cm2104-2324-groupproject-jp/final/ejs/public/scripts/account.js
+++ cm2104-2324-groupproject-jp/final/ejs/public/scripts/account.js
@@ -207,17 +207,18 @@ function buildReviewsMovieCard(movieInfo) {
       "<p>Year: " + releaseDate + "</p>" +
       "</div>" +
 
-      "<div class='review-extra' id='review-extra-" + id + "style=\"background-image: url('https://image.tmdb.org/t/p/original/" + movieBackdrop + "');\">" +
+      "<div class='review-extra' id='review-extra-" + id + "' style='background-image: url(" + movieBackdrop + ");'>" +
       "<h3>Your review</h3>" +
       "<p id='review-text-" + id + "'>Loved it then and love it now. It's aged like a fine wine. Say what you like about movies from the eighties but when they got it right, boy did they get it right. Still one of the most inventive, superbly performed, wonderfully written, expertly directed and wholly endearing comedies you'll ever watch. Even the great theme tune is memorable. A true classic in every sense of the word.</p>" +
       "<button class='review-change-button' onclick='changeReview(" + id + ")'>Edit</button>" +
       "</div>" +
-      "<div class='review-change' id='review-change-" + id + " style=\"background-image: url('https://image.tmdb.org/t/p/original/" + movieBackdrop + "');\">" +
+      "<div class='review-change' id='review-change-" + id + "' style='background-image: url(" + movieBackdrop + ");'>" +
       "<h3>Edit your review</h3>" +
       "<input type='text' class='change-review-textbox-" + id + "' id='change-review-textbox-" + id + "' placeholder='Enter your new review'>" +
       "<button class='review-change-button' onclick='saveReview(" + id + ")'>Save</button>" +
       "</div>" +
       "</div>";
+
 
   // Inserting the HTML into the watchlist movie card container
   $('.reviews-movie-card-container').append(htmlString);
