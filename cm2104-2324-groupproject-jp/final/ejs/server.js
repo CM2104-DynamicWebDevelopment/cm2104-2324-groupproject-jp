2eaa26b501a21e83389707dc7d676b11735df1c7
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2024-04-20 12:12:14 +0100

    test

--- cm2104-2324-groupproject-jp/final/ejs/server.js
+++ cm2104-2324-groupproject-jp/final/ejs/server.js
@@ -53,6 +53,56 @@ app.get('/', (req, res) => {
     res.render('pages/error');
 });
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+var users = {};
+
+//code to define the public "static" folder
+app.use(express.static('public'))
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function (username,msg) {
+        io.emit('chat message',username, msg);
+    });
+
+    socket.on('room message', function (room, username, msg) {
+        socket.join(room);
+        io.to(room).emit('chat message',username, msg);
+    });
+
+    socket.on('set username', function(username){
+        users[username] = socket.id;
+        socket.emit('set username', 'username has been set to '+username);
+    }); 
+
+    socket.on('private message', function (recipient, username, msg) {
+        var sendTo = users[recipient];
+        io.to(sendTo).emit('private message',username, msg);
+    });
+
+
+});
+
+http.listen(8080, function () {
+    console.log('listening on *:8080');
+});
+
+
+
+
+io.on('connection', function (socket) {
+     console.log('a user connected');
+     socket.on('disconnect', function () {
+         console.log('user disconnected');
+     });
+});
 
 // Route to render the myaccount.ejs page
 app.get('/myaccount', (req, res) => {
